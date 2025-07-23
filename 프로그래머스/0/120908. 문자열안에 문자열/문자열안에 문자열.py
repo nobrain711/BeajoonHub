d9de8a2060082b7543cb1b67030e395a15091f0e
DONGHWI JO <82422468+nobrain711@users.noreply.github.com>
2025-07-24 01:36:30 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.00 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
@@ -1,6 +1,2 @@
 def solution(str1, str2):
-    for i in range(len(str1)-(len(str2)-1)):
-        if str1[i:i+len(str2)]==str2:
-            return 1
-            break
-    return 2
+    return 1 if str2 in str1 else 2
